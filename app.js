88383f4bdb5455d6d97c27d9c60a267dd4fa41e7
Praveen Sadashiv Nayak <praveen.nayak@yash.com>
2020-12-09 12:03:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+//Sets the public folder as the external file folder
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req,res){
+    res.render("homepage");
+});
+
+app.get("/gamelist",function(req,res){
+    const games = [
+        {title: "Cricket", creator: "England"},
+        {title: "Hockey", creator: "India"},
+        {title: "BasketBall", creator: "US"}
+    ]
+      res.render("gamelist", {
+        gamesList: games
+    });
+  });
+
+//const port = process.env.port || 3000;
+app.listen("3000", function(){
+    console.log("Gaming website has started up! Made by Praveen Nayak...");
+});
+
+module.exports = app;
